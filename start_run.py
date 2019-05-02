9426bf822f8e82c8c005e9b68275b32f6a415241
Kristine Hagen <kristine@spacemaker.ai>
2019-05-02 18:17:57 +0200

    Initial structure for simulation

--- start_run.py
+++ start_run.py
@@ -4,11 +4,11 @@ from input_data.load_customers import load_customers, Customer
 from input_data.load_vendors import load_vendors, Vendor
 from input_data.products import load_product_spec
 from optimize.optimize import start_optimize
+from profit import calculate_profit_for_start_day
 from scenarios.load_scenarios import load_scenarios
-from simulation.profit import calculate_profit_for_start_day
 
 DETERMINISTIC = True
-SIMULATE_RESULTS = True
+SIMULATE_RESULTS = False
 NUMBER_OF_DAYS_IN_EACH_RUN = 5
 TIME_HORIZON = 15
 START_DAY = 1
@@ -19,41 +19,40 @@ def start_run():
     vendors = load_vendors()
     customers = load_customers()
     product_specs = load_product_spec()
+    start_day = START_DAY
+    end_day = start_day + NUMBER_OF_DAYS_IN_EACH_RUN
 
     if SIMULATE_RESULTS:
         scenarios = load_scenarios()
         number_of_runs = TIME_HORIZON - NUMBER_OF_DAYS_IN_EACH_RUN + 1
 
-    start_day = START_DAY
-    end_day = start_day + NUMBER_OF_DAYS_IN_EACH_RUN
+    else:
+        vendors_with_relevant_deliveries = _filter_out_deliveries_after_end_time(
+            vendors=vendors,
+            end_day=end_day,
+        )
+        customers_with_relevant_orders = _filter_out_order_out_of_time_scope(
+            customers=customers,
+            start_day=start_day,
+            end_day=end_day,
+        )
+        actions = start_optimize(
+            vendors=vendors_with_relevant_deliveries,
+            customers=customers_with_relevant_orders,
+            product_specs=product_specs,
+        )
+        profit_for_start_day = calculate_profit_for_start_day(
+            vendors=vendors_with_relevant_deliveries,
+            customers=customers_with_relevant_orders,
+            product_specs=product_specs,
+            actions=actions,
+            start_day=start_day,
+        )
 
-    vendors_with_relevant_deliveries = _filter_out_deliveries_out_of_time_scope(
-        vendors=vendors,
-        end_day=end_day,
-    )
-    customers_with_relevant_orders = _filter_out_order_out_of_time_scope(
-        customers=customers,
-        start_day=start_day,
-        end_day=end_day,
-    )
-
-    actions = start_optimize(
-        vendors=vendors_with_relevant_deliveries,
-        customers=customers_with_relevant_orders,
-        product_specs=product_specs,
-    )
-
-    profit_for_start_day = calculate_profit_for_start_day(
-        vendors=vendors_with_relevant_deliveries,
-        customers=customers_with_relevant_orders,
-        product_specs=product_specs,
-        actions=actions,
-        start_day=start_day,
-    )
-    stop = 4
+    stop = 6
 
 
-def _filter_out_deliveries_out_of_time_scope(vendors: List[Vendor], end_day: int) -> List[Vendor]:
+def _filter_out_deliveries_after_end_time(vendors: List[Vendor], end_day: int) -> List[Vendor]:
     vendors_with_only_relevant_deliveries = [
         Vendor(
             id=vendor.id,
